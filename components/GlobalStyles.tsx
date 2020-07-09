e6bc8e54278ac5839d8175c56b13d5e6a98dc410
Oli Legg <oliglegg@hotmail.co.uk>
2020-07-09 23:22:57 +0100

    Starting to remove styled and mobx

---
+++ components/GlobalStyles.tsx
@@ -0,0 +1,10 @@
+// styles/global.js
+import css from 'styled-jsx/css'
+
+const GlobalStyles = css.global`
+    body {
+        margin: 0;
+    }
+`
+
+export default GlobalStyles;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,35 +1,32 @@
-import { StoreProvider, useStore } from '../Shared/StoreContext';
 import { SignalRReconnect } from '../Shared/SignalR';
 import React, {useEffect} from 'react';
-import AppProps from 'next/app';
+import App from 'next/app';
 import GlobalStyles from '../components/GlobalStyles';
 
-const MyApp = (props: AppProps<{}>) => {
+const MyApp = ({Component, pageProps}) => {
     
     useEffect(() => {
-        const store = useStore();
+        // const store = useStore();
 
         const html = document.getElementsByTagName('html');
         if (html && html.length > 0) {
             html[0].setAttribute('lang', 'en');
         }
         const timer = setTimeout(async () => {
-            await SignalRReconnect(store);
+            // await SignalRReconnect(store);
         });
 
         return () => clearTimeout(timer)
         
     }, []);
 
-
-    const {Component, pageProps} = props;
     return (
-        <StoreProvider>
+        <>
             <Component {...pageProps} />
             <style jsx global>
                 {GlobalStyles}
             </style>
-        </StoreProvider>
+        </>
     )
 };
 
