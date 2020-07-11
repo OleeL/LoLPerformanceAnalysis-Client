e7c7fcf7581346ffb4ecbee2f9ed171488e7c632
Oli Legg <oliglegg@hotmail.co.uk>
2020-07-11 23:10:35 +0100

    Adding bulma back

--- components/GlobalStyles.tsx
+++ components/GlobalStyles.tsx
@@ -1,6 +1,8 @@
 // styles/global.js
 import css from 'styled-jsx/css'
 
+export const Primary = "#00b0e0";
+
 const GlobalStyles = css.global`
     body {
         margin: 0;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,55 +1,60 @@
-import React, { useState, useEffect, FormEvent } from "react";
-import styled from 'styled-components';
 import { SendGetSummoner } from "../Shared/Requests";
-import { useStore } from "../Shared/StoreContext";
+import { useStore } from "../Shared/Store";
 import { useRouter } from 'next/router'
-import { TStore } from "../Shared/Store";
-import { useObserver } from "mobx-react-lite";
+import { FC, useState, FormEvent } from "react";
+import { Primary } from "../components/GlobalStyles";
 
-const Primary = "#00b0e0";
-
-const Page = styled.div`
-    padding: 30vh 0;
+const Page = `
+    div {
+        padding: 30vh 0;
+    }
 `;
 
-const Title = styled.h1`
-    text-align: center;
+const Title = `
+    h1 {
+        text-align: center;
+    }
 `
 
-const SummonerInput = styled.input`
-    margin: 0px 0px 10px;
+const SummonerInput = `
+    input {
+        margin: 0px 0px 10px;
+    }
 `
 
-const ImageContainer = styled.div`
-    text-align: center;
-    width: 100vw;
-    overflow: hidden;
-    margin: 4px 0px 10px 0px;
-    padding: 4px;
+const Image = `
+    img {
+        margin: 0 auto;
+        padding: 0;
+        
+        position: relative;
+
+        border-radius: 400px;
+        box-shadow: 0px 0px 4px 2px rgba(0,0,0,0.5);
+
+        width: 100px;
+        
+        z-index:1;
+    }
+
+    div {
+        text-align: center;
+        width: 100vw;
+        overflow: hidden;
+        margin: 4px 0px 10px 0px;
+        padding: 4px;
+    }
 `
 
-const Image = styled.img`
-    
-    margin: 0 auto;
-    padding: 0;
-    
-    position: relative;
-
-    border-radius: 400px;
-    box-shadow: 0px 0px 4px 2px rgba(0,0,0,0.5);
-
-    width: 100px;
-    
-    z-index:1;
-`
-
-const Footer = styled.footer`
-    height: 0;
-    position: absolute;
-    bottom: 0; 
-    width: 100%;
-    background: ${Primary};
-    color: white;
+const Footer = `
+    footer {
+        height: 0;
+        position: absolute;
+        bottom: 0; 
+        width: 100%;
+        background: ${Primary};
+        color: white;
+    }
 `
 
 interface State {
@@ -62,54 +67,62 @@ const index = () => {
     const [serverRegion, setServerRegion] = useState("EUW");
 
     const Router = useRouter();
-    const store: TStore = useStore();
+    const store = useStore();
     
-    const HandleForm = async (e: React.FormEvent<HTMLFormElement>) => {
+    const HandleForm = async (e: FormEvent<HTMLFormElement>) => {
         e.preventDefault();
         Router.replace("/"+serverRegion+"/"+summonerName);
     }
-
-    return useObserver(() => {
-        const connected = store.connected;
-        return (
-            <>
-                <Page className="columns is-mobile is-multiline is-centered is-vcentered">
-                    <DrawTitle />
-                    <ImageContainer>
-                        <Image src={"data/images/Olangutan.jpg"} alt="Olangutan" />
-                    </ImageContainer>
-                    <form className="columns is-mobile is-multiline is-centered is-vcentered" onSubmit={e => HandleForm(e)}>
-                        <DrawInput value={summonerName} setter={setSummonerName}/>
-                        <DrawServerList value={serverRegion} setter={setServerRegion} />
-                        <DrawButtonSubmit />
-                    </form>
-                </Page>
-                <DrawFooter />
-            </>
-        );
-    })
+    const {connected} = store;
+    return (
+        <>
+            <div className="columns is-mobile is-multiline is-centered is-vcentered">
+                <DrawTitle />
+                <DrawImage />
+                <form className="columns is-mobile is-multiline is-centered is-vcentered" onSubmit={e => HandleForm(e)}>
+                    <DrawInput value={summonerName} setter={setSummonerName}/>
+                    <DrawServerList value={serverRegion} setter={setServerRegion} />
+                    <DrawButtonSubmit />
+                </form>
+                <style jsx>{Page}</style>
+            </div>
+            <DrawFooter />
+        </>
+    );
 }
 
-const DrawTitle: React.FC = () => 
+const DrawTitle: FC = () => 
     <div className="column is-full">
-        <Title className="title">Olangutan Analytics</Title>
+        <h1 className="title">Olangutan Analytics</h1>
+        <style jsx>{Title}</style>
     </div>
 
-const DrawFooter: React.FC = () => 
-    <Footer className="footer">
+const DrawImage: FC = () =>
+    <>
+        <DrawTitle />
+        <div>
+            <img src={"data/images/Olangutan.jpg"} alt="Olangutan"/>
+            <style jsx>{Image}</style>
+        </div>
+    </>
+
+const DrawFooter: FC = () => 
+    <footer className="footer">
         <div className="content has-text-centered">
             <p>
                 <b>Olangutan Analytics</b> isn't endorsed by Riot Games and doesn't reflect the views or opinions of Riot Games or anyone officially involved in producing or managing Riot Games properties. Riot Games, and all associated properties are trademarks or registered trademarks of Riot Games, Inc.
+                <style jsx>{Footer}</style>
             </p>
         </div>
-    </Footer>
+    </footer>
 
-const DrawInput: React.FC<State> = ({value, setter}) =>
+const DrawInput: FC<State> = ({value, setter}) =>
     <div className="column is-full">
-        <SummonerInput className="input is-rounded is-focused" value={value} onChange={e => setter(e.target.value)} type="text" placeholder="Summoner Name" />
+        <input className="input is-rounded is-focused" value={value} onChange={e => setter(e.target.value)} type="text" placeholder="Summoner Name" />
+        <style jsx>{SummonerInput}</style>
     </div>
 
-export const DrawServerList: React.FC<State> = ({value, setter}) =>
+export const DrawServerList: FC<State> = ({value, setter}) =>
         <div className="column is-one-quarter">
             <div className="select">
                 <select value={value} onChange={e => setter( e.target.value )}>
@@ -128,7 +141,7 @@ export const DrawServerList: React.FC<State> = ({value, setter}) =>
             </div>
         </div>
 
-const DrawButtonSubmit: React.FC = () => 
+const DrawButtonSubmit: FC = () => 
     <div className="column">
         <button className="button is-primary is-fullwidth" type="submit">Submit</button>
     </div>
