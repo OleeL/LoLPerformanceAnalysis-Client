7cea88e54f91e54d0e4751bac03b4160d7a70182
Oli Legg <oliglegg@hotmail.co.uk>
2020-07-11 23:10:48 +0100

    Including zustand

--- Shared/Store.ts
+++ Shared/Store.ts
@@ -1,27 +1,18 @@
 
-import {observable} from 'mobx';
+import create from 'zustand';
 import { ISummoner } from './GameInterfaces';
 
-export const createStore = () => {
-    const store = observable({
-        connected: false,
-        setConnected: (status: boolean) => store.connected = status,
-        receivedData: false,
-        setReceivedData: (received: boolean) => store.receivedData = received,
-        summoner: null as ISummoner,
-        region: null as string,
-        setSummoner: (summoner: ISummoner, region: string) => {
-            const update = {
-                summoner: summoner,
-                region: region,
-                receivedData: true
-            }
-            Object.assign(store, update);
-            console.log(store.summoner);
+export const [useStore] = create (set => ({
+    connected: false,
+    receivedData: false,
+    summoner: null as ISummoner,
+    region: null as string,
+    setSummoner: (summoner: ISummoner, region: string) => {
+        const update = {
+            summoner: summoner,
+            region: region,
+            receivedData: true
         }
-    });
-    
-    return store;
-};
-
-export type TStore = ReturnType<typeof createStore>;
+        Object.assign(useStore, update);
+    }
+}));
