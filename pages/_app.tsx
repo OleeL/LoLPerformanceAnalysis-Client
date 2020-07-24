350442b8e70864135ff3abee65280473de419dc1
Oli Legg <oliglegg@hotmail.co.uk>
2020-07-25 00:29:13 +0100

    Dynamic document title

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,15 +4,17 @@ import React, {useEffect} from 'react';
 import GlobalStyles from '../components/GlobalStyles';
 import "../sass/main.scss";
 
-const MyApp = ({Component, pageProps}) => {
+const MyApp = ({Component, pageProps, router}) => {
 
     const {setConnected} = useStore();
-
     const triggerConnected = (connected: boolean) => setConnected(connected);
     
     useEffect(() => {
         const html = document.getElementsByTagName('html');
         document.title = "Olangutan Analytics"
+        if (router.query.region && router.query.summoner) {
+            document.title = router.query.region + " - " + router.query.summoner;
+        }
         if (html && html.length > 0) {
             html[0].setAttribute('lang', 'en');
         }
